f3df3085a14e3283ddfdc8de5cbf53f1b3aa590f
gunwoong <gwj0421@gmail.com>
2025-04-22 18:00:46 +0900

    Init : first commit

---
+++ src/main/java/programmers/nbe5_7_1_8bit/Nbe5718bitApplication.java
@@ -0,0 +1,12 @@
+package programmers.nbe5_7_1_8bit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nbe5718bitApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Nbe5718bitApplication.class, args);
+  }
+}
---
+++ src/test/java/programmers/nbe5_7_1_8bit/Nbe5718bitApplicationTests.java
@@ -0,0 +1,11 @@
+package programmers.nbe5_7_1_8bit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nbe5718bitApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
