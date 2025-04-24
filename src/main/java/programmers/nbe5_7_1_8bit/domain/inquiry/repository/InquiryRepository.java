06b8ba1648ef37c4922d1fa71f6177ff8d479be8
gunwoong <gwj0421@gmail.com>
2025-04-24 22:56:35 +0900

    Feat: 문의 & 문의 답글 기능 추가
    - 문의 CRUD(soft) 구현
    - 문의 답글 CRUD(soft) 구현
    - 문의 Page 조회
    
    softDelete는 @Filter와 @FilterDef를 활용하여 구현

--- src/main/java/programmers/nbe5_7_1_8bit/domain/inquiry/repository/InquiryRepository.java
+++ src/main/java/programmers/nbe5_7_1_8bit/domain/inquiry/repository/InquiryRepository.java
@@ -1,8 +1,28 @@
 package programmers.nbe5_7_1_8bit.domain.inquiry.repository;
 
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import programmers.nbe5_7_1_8bit.domain.inquiry.entity.Inquiry;
+import programmers.nbe5_7_1_8bit.domain.inquiry.entity.InquiryAnswerDto;
+import programmers.nbe5_7_1_8bit.domain.inquiry.entity.InquiryDto;
 
 public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
 
+  @Query(
+      "select new programmers.nbe5_7_1_8bit.domain.inquiry.entity.InquiryDto(i.title,i.question,i.inquiryAnswer.answer,i.createdAt,i.updatedAt) from Inquiry i where i.id = :id")
+  InquiryDto findInquiryDtoById(@Param("id") Long id);
+
+  @Query(
+      "select new programmers.nbe5_7_1_8bit.domain.inquiry.entity.InquiryDto(i.id,i.title,i.createdAt,i.updatedAt) "
+          + "from Inquiry i order by i.createdAt desc")
+  Page<InquiryDto> findPageForManager(Pageable pageable);
+
+  @Query("select new programmers.nbe5_7_1_8bit.domain.inquiry.entity.InquiryAnswerDto(i.inquiryAnswer.id,i.inquiryAnswer.answer) from Inquiry i where i.id = :id")
+  InquiryAnswerDto findInquiryAnswerDtoById(@Param("id") Long id);
+
+  List<Inquiry> findAllByMember_Id(Long id);
 }
---
+++ src/main/java/programmers/nbe5_7_1_8bit/domain/inquiry/service/InquiryAnswerServiceImpl.java
@@ -0,0 +1,46 @@
+package programmers.nbe5_7_1_8bit.domain.inquiry.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import programmers.nbe5_7_1_8bit.domain.inquiry.entity.InquiryAnswerDto;
+import programmers.nbe5_7_1_8bit.domain.inquiry.repository.InquiryAnswerRepository;
+import programmers.nbe5_7_1_8bit.domain.inquiry.repository.InquiryRepository;
+import programmers.nbe5_7_1_8bit.global.exception.IllegalInquiryIdException;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class InquiryAnswerServiceImpl implements InquiryAnswerService {
+
+  private final InquiryAnswerRepository inquiryAnswerRepository;
+  private final InquiryRepository inquiryRepository;
+
+  @Override
+  public void save(InquiryAnswerDto inquiryAnswerDto) {
+    inquiryRepository
+        .findById(inquiryAnswerDto.getId())
+        .ifPresentOrElse(
+            inquiry -> inquiry.answerInquiry(InquiryAnswerDto.of(inquiryAnswerDto.getAnswer())),
+            () -> {
+              throw new IllegalInquiryIdException();
+            });
+  }
+
+  @Override
+  public InquiryAnswerDto findById(Long id) {
+    return inquiryRepository.findInquiryAnswerDtoById(id);
+  }
+
+  @Override
+  public void update(InquiryAnswerDto inquiryAnswerDto) {
+    inquiryAnswerRepository
+        .findById(inquiryAnswerDto.getId())
+        .ifPresent(inquiryAnswer -> inquiryAnswer.update(inquiryAnswerDto));
+  }
+
+  @Override
+  public void softRemove(Long id) {
+    inquiryAnswerRepository.findById(id).ifPresent(inquiryAnswer -> inquiryAnswer.softDelete());
+  }
+}
