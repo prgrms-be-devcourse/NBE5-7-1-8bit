3248dd66042a7011da1ca94e835f05a7730a0a68
Kim, Seokwan <sukwan1106@gmail.com>
2025-04-25 00:40:51 +0900

    Feat: ProductException 상품 예외 처리

---
+++ src/main/java/programmers/nbe5_7_1_8bit/domain/product/exception/ProductException.java
@@ -0,0 +1,36 @@
+package programmers.nbe5_7_1_8bit.domain.product.exception;
+
+public class ProductException extends RuntimeException {
+
+  public ProductException(String message) {
+    super(message);
+  }
+
+  public static class ProductNotFoundException extends ProductException {
+
+    public ProductNotFoundException() {
+      super("존재하지 않는 상품입니다.");
+    }
+  }
+
+  public static class RemovedProductException extends ProductException {
+
+    public RemovedProductException() {
+      super("삭제된 상품입니다.");
+    }
+  }
+
+  public static class ProductImageNotFoundException extends ProductException {
+
+    public ProductImageNotFoundException() {
+      super("해당 제품에 이미지가 없습니다.");
+    }
+  }
+
+  public static class ImageReadException extends ProductException {
+
+    public ImageReadException(String filename) {
+      super("파일을 읽을 수 없습니다: " + filename);
+    }
+  }
+}
