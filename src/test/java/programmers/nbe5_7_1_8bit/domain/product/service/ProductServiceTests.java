ab23c071543e2c7b54c5eb5efaa9aa3e4b2a950a
gunwoong <gwj0421@gmail.com>
2025-04-25 16:38:44 +0900

    Test fix: merge 과정에서 발생된 Test 수정

--- src/test/java/programmers/nbe5_7_1_8bit/domain/product/service/ProductServiceTests.java
+++ src/test/java/programmers/nbe5_7_1_8bit/domain/product/service/ProductServiceTests.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import jakarta.persistence.EntityManager;
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +11,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import programmers.nbe5_7_1_8bit.domain.product.dto.ProductRequestDto;
 import programmers.nbe5_7_1_8bit.domain.product.dto.ProductResponseDto;
 import programmers.nbe5_7_1_8bit.domain.product.entity.Product;
-import programmers.nbe5_7_1_8bit.domain.product.exception.ProductException.ProductNotFoundException;
 import programmers.nbe5_7_1_8bit.domain.product.exception.ProductException.RemovedProductException;
 import programmers.nbe5_7_1_8bit.domain.product.repository.ProductRepository;
 
@@ -70,9 +68,9 @@ class ProductServiceTests {
     assertThat(retrievedProduct.getName()).isEqualTo("라떼");
     assertThat(retrievedProduct.getPrice()).isEqualTo(2000);
     assertThat(retrievedProduct.getStock()).isEqualTo(20);
-    
+
   }
-  
+
   @Test
   @DisplayName("제품 수정 테스트")
   void 제품_수정_성공() throws Exception {
@@ -92,7 +90,8 @@ class ProductServiceTests {
         .build();
 
     //when
-    ProductResponseDto updatedProduct = productService.updateProduct(createdProduct.getId(), updateRequest);
+    ProductResponseDto updatedProduct = productService.updateProduct(createdProduct.getId(),
+        updateRequest);
 
     //then
     assertThat(updatedProduct.getName()).isEqualTo("아이스티티");
@@ -118,61 +117,9 @@ class ProductServiceTests {
     productService.deleteProduct(createdProduct.getId());
 
     //then
-    assertThrows(IllegalStateException.class, () -> productService.getProduct(createdProduct.getId()));
-    assertThrows(IllegalArgumentException.class, () -> productService.getProduct(createdProduct.getId()));
-
+    assertThrows(RemovedProductException.class,
+        () -> productService.getProduct(createdProduct.getId()));
   }
 
-//  @Test
-//  @DisplayName("제품 목록 조회 테스트")
-//  void 제품_목록_조회_성공() throws Exception {
-//    //given
-//    ProductRequestDto.builder()
-//        .name("1")
-//        .price(11)
-//        .stock(111)
-//        .build();
-//
-//    ProductRequestDto.builder()
-//        .name("2")
-//        .price(22)
-//        .stock(222)
-//        .build();
-//
-//    //when
-//    List<ProductResponseDto> products = productService.getAllProduct();
-//
-//    //then
-//    assertThat(products.size()).isEqualTo(2);
-//    assertThat(products.get(0).getName()).isEqualTo("1");
-//    assertThat(products.get(1).getName()).isEqualTo("2");
-//
-//
-//
-//
-//
-//  }
-  
-  
-  
-//  @Test
-//  @DisplayName("연관관계 비교")
-//  void testOneToMany() throws Exception {
-//    Query orderQuery = entityManager.createQuery("SELECT o FROM Order o");
-//    orderQuery.getResultList();
-//
-//
-//    Query orderProductQuery = entityManager.createQuery("SELECT op FROM OrderProduct op");
-//    orderProductQuery.getResultList();
-//
-//  }
-//
-//  @Test
-//  @DisplayName("연관관계 성능 비교")
-//  void testManyToOne() throws Exception {
-//    Query orderProductQuery = entityManager.createQuery("SELECT op FROM OrderProduct op JOIN FETCH op.order");
-//    orderProductQuery.getResultList();
-//
-//  }
 
 }
